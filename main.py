797aa3ada5c41888a47f7d04f795f83bab4f51b3
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2023-09-26 22:36:21 +0700

    Stuff

--- main.py
+++ main.py
@@ -1,5 +1,35 @@
 # Abandon all hope, ye who enter here
+import console
+
+
+def show_menu(message: str, items: list[str]) -> int:
+    if message:
+        print(message)
+
+    for i, item in enumerate(items):
+        print(f"{i + 1} - {item}")
+
+    while True:
+        try:
+            choice = int(input("Choice: ")) - 1
+            if choice in range(len(items)):
+                return choice
+            else:
+                raise ValueError()
+        except ValueError:
+            print("Invalid choice.")
+
+
 def main() -> int:
+    while True:
+        console.clear()
+
+        match show_menu("Main Menu", ["Calculate AABB from OBJ mesh", "Exit"]):
+            case 0:
+                print("TODO: Implement")  # TODO: Implement
+            case 1:
+                return 0
+
     return 0
 
 
